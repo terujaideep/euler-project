ab1783d7a896d82e616680affa2b2c3bff37165a
terujaideep <jaideep.teru@students.iiit.ac.in>
2018-12-02 22:06:19 +0530

    i added

---
+++ e2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+long long int fibo(int i){
+        if(i==0){
+                return 0;}
+        if(i==1){
+                return 2;
+        }
+                else{
+                        return 4*fibo(i-1) + fibo(i-2);
+                }
+}
+int main(void)
+{
+	long long int t;
+	scanf("%lld",&t);
+	while(t--)
+	{
+		long long int n,sum=0,b;
+		scanf("%lld",&n);
+		for(long long int i=1;;i++)
+		{
+			b=fibo(i);
+			if(b<=n){
+			sum+=b;}
+			else{
+				break;
+			}
+		}
+		printf("%lld\n",sum);
+	}
+}
+	
+
