97a59e56094b38c44e5d083391e4aab1f7c83e65
terujaideep <jaideep.teru@students.iiit.ac.in>
2018-12-07 16:12:14 +0530

    add

--- e2.c
+++ e2.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-long long int fibo(int i){
+/*long long int fibo(int i){
         if(i==0){
                 return 0;}
         if(i==1){
@@ -8,25 +8,30 @@ long long int fibo(int i){
                 else{
                         return 4*fibo(i-1) + fibo(i-2);
                 }
-}
+}*/
 int main(void)
 {
 	long long int t;
 	scanf("%lld",&t);
 	while(t--)
-	{
-		long long int n,sum=0,b;
+	{	
+		long long int n,sum=0,b,first1=2,next,first2=0;
 		scanf("%lld",&n);
 		for(long long int i=1;;i++)
 		{
-			b=fibo(i);
-			if(b<=n){
-			sum+=b;}
+			next=4*first1+first2;
+			first2=first1;
+			first1=next;
+			if(next<=n){
+				sum+=next;
+			}
 			else{
 				break;
 			}
 		}
-		printf("%lld\n",sum);
+
+		printf("%lld\n",sum+2);
 	}
+	return 0;
 }
 
