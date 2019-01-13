b704674d4d51bfe48992146bbb41f3446fba51ff
terujaideep <jaideep.teru@students.iiit.ac.in>
2019-01-13 12:13:37 +0530

    18

---
+++ e18.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int max(int a,int b){
+	if(a>b){
+		return a;
+	}
+	else{
+		return b;}
+}
+struct jai{
+	int A[15];
+}B[15];
+int main(void)
+{
+	int t;
+	scanf("%d",&t);
+	while(t--){
+	int n; scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+		scanf("%d",&B[i].A[j]);}
+	}
+	for(int i=n-2;i>=0;i--){
+		for(int j=0;j<i+1;j++){
+		B[i].A[j]=B[i].A[j]+max(B[i+1].A[j],B[i+1].A[j+1]);
+		}
+	}
+	printf("%d\n",B[0].A[0]);
+	}
+	return 0;
+}
+
+
+
+
+
